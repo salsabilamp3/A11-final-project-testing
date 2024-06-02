a7ee4d5f45d9240110c52f9a81e2e8c206751c9f
salsabilamp3 <salsabilamp3@gmail.com>
2024-06-02 12:46:29 +0700

    add api testing base and 1 update script

--- src/test/java/stepdefinitions/apitesting/ApiUserSteps.java
+++ src/test/java/stepdefinitions/apitesting/ApiUserSteps.java
@@ -1,5 +1,49 @@
 package stepdefinitions.apitesting;
 
-public class ApiUserSteps {
+import Model.apitesting.User;
+import helper.SetUpEndPoint;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import testlogic.apitesting.CreateUserTest;
+import testlogic.apitesting.DeleteUserTest;
+import testlogic.apitesting.GenericProcessAPI;
+import testlogic.apitesting.GetUserTest;
+import testlogic.apitesting.UpdateUserTest;
 
+public class ApiUserSteps {
+    GetUserTest apiGet;
+    CreateUserTest apiCreate;
+    UpdateUserTest apiUpdate;
+    DeleteUserTest apiDelete;
+    User dataTestUpdate;
+
+    public ApiUserSteps(){
+        apiGet = new GetUserTest();
+        apiCreate = new CreateUserTest();
+        apiUpdate = new UpdateUserTest();
+        apiDelete = new DeleteUserTest();
+    }
+
+    @Given("Prepare url for {string}")
+    public void prepare_url_for(String endpoint){
+        SetUpEndPoint.prepareURL(endpoint);
+    }
+
+    @Then("validate status response code is equals {int}")
+    public void validate_status_response_code_is_equals(int statusCode){
+        GenericProcessAPI.validateStatusCode(apiUpdate.getResponse(), statusCode);
+    }
+
+    /* Step Update User */
+    @Then("Hit api update user by id {string}")
+    public void hit_api_update_user_by_id(String userId){
+        dataTestUpdate = UpdateUserTest.prepareBodyUpdateAll();
+        String url = SetUpEndPoint.getURL();
+        apiUpdate.putUser(url, userId, dataTestUpdate);
+    }
+
+    @Then("validate response body updated user")
+    public void validate_response_body_updated_user() {
+        apiUpdate.checkResponseBodyUpdatedUser(dataTestUpdate);
+    }
 }
